0dba741d29d53f8be190fda3ee2f9d5ee5da54ef
cyberumair <umairshakoor.pro@gmail.com>
2025-08-12 15:32:33 +0500

    Moved Password Gen & Qr Code Gen to CyberPy Repo

---
+++ Projects/01. Learning/HTTP Server/main.py
@@ -0,0 +1,1 @@
+# To start a python http server just run : `python -m http.server` and you're http server starts
--- Projects/05.Tools/Qr Code Gen/main.py
+++
@@ -1,5 +0,0 @@
-# pip install qrcode qrcode[pil]
-
-import qrcode as qr
-img = qr.make('https://umairshakoor.vercel.app')
-img.save('main.png')
